b605058fb61d181defd0fdf3c75aaf7416fa3b95
gitlin-lin <LL1472583690928@163.com>
2024-09-28 19:46:14 +0800

    Add files via upload

---
+++ first-site-my/scripts/main.js
@@ -0,0 +1,41 @@
+
+let myImage = document.querySelector("img");
+
+myImage.onclick = function () {
+  let mySrc = myImage.getAttribute("src");
+  if (mySrc === "images\OIP-C.jpg") {
+    myImage.setAttribute("src", "images\web-image.jpg");
+  } else {
+    myImage.setAttribute("src", "images\OIP-C.jpg");
+  }
+};
+
+let myButton = document.querySelector("button");
+let myHeading = document.querySelector("h1");
+
+function setUserName() {
+    let myName = prompt("请输入你的名字。");
+    localStorage.setItem("name", myName);
+    myHeading.textContent = "Mozilla 酷毙了，" + myName;
+  }
+
+if (!localStorage.getItem("name")) {
+    setUserName();
+} else {
+  let storedName = localStorage.getItem("name");
+  myHeading.textContent = "Mozilla 酷毙了，" + storedName;
+}
+
+myButton.onclick = function () {
+  setUserName();
+};
+
+function setUserName() {
+    let myName = prompt("请输入你的名字。");
+    if (!myName) {
+      setUserName();
+    } else {
+      localStorage.setItem("name", myName);
+      myHeading.textContent = "Mozilla 酷毙了，" + myName;
+    }
+  }
